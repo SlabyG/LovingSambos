77a3a04d218f9e753d6b8ccf62a19eeea84eb6a2
SlabyG <gabriel17jh@gmail.com>
2021-09-05 06:38:04 +0000

    Calculate surplus data

--- run.py
+++ run.py
@@ -57,7 +57,6 @@ def validate_data(values):
 
     return True
 
-
 def update_sales_worksheet(data):
     """
     update sales worksheet, add new row with  the list data provided.
@@ -78,7 +77,13 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+
+    surplus_data =[]
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
 
 
 
@@ -89,7 +94,8 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    print(new_surplus_data)
 
 
 
