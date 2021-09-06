33e7114a0ffd511f571993bab7b1e0a34dd8bbc3
SlabyG <gabriel17jh@gmail.com>
2021-09-06 10:46:45 +0000

    added the get stock value  from the chalange and incorporated it into the project

--- run.py
+++ run.py
@@ -133,6 +133,15 @@ def calculate_stock_data(data):
 
     return new_stock_data
 
+def get_stock_values(data):
+    """
+    This function will get the stock values 
+    """
+    headings = SHEET.worksheet("stock").row_values(1)
+    return {key : value for key, value in zip(headings, data)}
+
+    
+    return stock_values
 
 
 def main():
@@ -147,6 +156,7 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    stock_values = get_stock_values(stock_data)
 
 
 
